5d1af06e0178a54aa29afa27c730cc198b92cf5d
Berke Can <berkeyuz@outlook.com>
2025-10-24 20:35:04 -0700

    New collision method with command interface

--- grid.cpp
+++ grid.cpp
@@ -10,6 +10,7 @@
 #include "player.h"
 #include "field.h"
 #include "points.h"
+#include "CollisionCommand.h"
 
 #pragma comment(lib, "Winmm.lib")
 #pragma comment(lib, "ole32.lib")
@@ -137,59 +138,11 @@ void moveLoop(HWND hwnd) {
         }
 }
 
-
-// Command interface
-class Icommand 
+// New CollisionCheck method with Command interface
+void Grid::collisionCheck()
 {
-	public:
-		virtual void execute() = 0;
-		virtual ~ICommand() = default;
-};
-
-// Concrete command for Collision check
-class CollisionCommand : public ICommand 
-{
-	Character& character;
-	ActivePoint& activePoint;
-	GameField& gameField;
-
-	public:
-		CollisionCommand(Character& c, ActivePoint& p, GameField& gf) 
-			:character(c), activePoint(p), gameField(gf) {}
-
-		void exectue() override
-		{
-			if (character.getX() == activePoint.getX() &&
-				character.getY() --activePoint.getY() &&
-				!activePoint.checkFollowing()) {
-				points* tailSeg = new points(activePoint.getX(), activePoint.getY());
-            object* head = &character;
-            activePoint.collect(nullptr);
-
-            if (!character.firstPoint()) 
-			{
-                tailSeg->collect(character.getTail());
-                gameField.updateTile(tailSeg->getX(), tailSeg->getY(), tailSeg->getColor(), gameField.getID());
-            } 
-			else 
-			{
-                tailSeg->collect(head);
-            }
-
-            character.collect(tailSeg);
-            createPoint(activePoint, gameField.getX(), gameField.getY());
-			}
-		}
-};
-
-// New collision check method with Command Design Pattern
-void Grid::collisionCheck() 
-{
-	for (auto& point : activePoints)
-	{
-		CollisionCommand cmd(character, point, gameField);
-		cmd.execute();
-	}
+	CollisionCommand cmd(character, activePoint, gameField);
+	cmd.execute();
 }
 
 
