9e7af0cbebffe9a78fb665952b065f926ce1ec8b
Sharderai <shadowstardiamond@yahoo.com>
2025-11-02 17:35:10 -0800

    fixed minor syntax issues, made class name same as file name, made methods public, and removed virtual destructor

--- GameStateI.h
+++ GameStateI.h
@@ -1,14 +1,16 @@
 #ifndef GAME_STATE_
 #define GAME_STATE_
 
-class GameState {
-    // Method to let player know if state is an active type (true) or inactive type (false)
-    virtual bool getPlayState() = 0;
+class GameStateI {
+    public:
+        // Method to let player know if state is an active type (true) or inactive type (false)
+        virtual bool getPlayState() = 0;
 
-    // Returns movement rule associated with the current state
-    virtual bool canMove() = 0;
+        // Returns movement rule associated with the current state
+        virtual bool canMove() = 0;
 
-    //returns object color associated with the current state
-    virtual COLORREF getColor() = 0;
-    virtual ~GameState(){}
-}
+        //returns object color associated with the current state
+        virtual COLORREF getColor() = 0;
+};
+
+#endif
